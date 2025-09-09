b14cce5cdefc95f93580d037212edb4cbdb5616a
youness anflous <anayouness51@gmail.com>
2025-09-09 18:44:08 +0100

    finish the ex02 about reference

---
+++ HI_THIS_IS_BRAIN/main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yanflous <yanflous@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/09 18:42:52 by yanflous          #+#    #+#             */
+/*   Updated: 2025/09/09 18:42:55 by yanflous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <iostream>
+
+int main()
+{
+    std::string string;
+    std::string *stringPTR;
+    
+    string = "HI THIS IS BRAIN";
+    stringPTR = &string;
+    std::string& stringREF = string;
+
+    std::cout << "The memory address of the string variable"
+            << ": " << &string << std::endl;
+    std::cout << "The memory address held by stringPTR"
+            << ": " << &stringPTR << std::endl;
+    std::cout << "The memory address held by stringREF"
+            << ": " << &stringREF << std::endl;
+
+    std::cout << "---------------------------------------------" << std::endl;
+
+    std::cout << "The value of the string variable -> " << string << std::endl;
+    std::cout << "The value pointed to by stringPTR -> " << *stringPTR << std::endl;
+    std::cout << "The value pointed to by stringREF-> " << stringREF << std::endl;
+
+
+}
