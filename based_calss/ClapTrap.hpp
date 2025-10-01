44c73ca7900398eb9b5e6a2d84c9210b28c69f0a
youness anflous <anayouness51@gmail.com>
2025-10-01 09:02:08 +0100

    create based class to use it latter to inherit from

---
+++ based_calss/ClapTrap.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yanflous <yanflous@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/26 14:21:46 by yanflous          #+#    #+#             */
+/*   Updated: 2025/09/26 14:21:47 by yanflous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class	ClapTrap
+{
+	private:
+		std::string name;
+		unsigned int hit_points;
+		unsigned int energy_points;
+		unsigned int attack_damage;
+
+	public:
+	
+		ClapTrap();
+		ClapTrap(const std::string &name);
+		ClapTrap(const ClapTrap &copy);
+		ClapTrap &operator=(const ClapTrap &copy);
+		~ClapTrap();
+		
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+		
+};
+
+# endif
